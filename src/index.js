7f98204864331115104dcddc07dbfb4774ef4944
Thej <tkodiyat01@gmail.com>
2021-03-12 20:34:51 -0800

    put everything inside cards

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ const App = () => {
     return (
         <div className="ui container comments">
             <AprrovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure?
+                </div></AprrovalCard>
+            <AprrovalCard>
                 <CommentDetail
                 author='Sam' 
                 timeAgo='Today at 4:45PM' 
@@ -16,18 +21,22 @@ const App = () => {
                 avatar={faker.image.image()}
                 />
             </AprrovalCard>
-            <CommentDetail
-             author='Alex' 
-             timeAgo='Today at 2:30PM' 
-             content='I like the subject'
-             avatar={faker.image.image()}
-            />
-            <CommentDetail
-             author='Janes' 
-             timeAgo='Yesterday at 4:45PM' 
-             content='I like the writing'
-             avatar={faker.image.image()}
-            />
+            <AprrovalCard>
+                <CommentDetail
+                author='Alex' 
+                timeAgo='Today at 2:30PM' 
+                content='I like the subject'
+                avatar={faker.image.image()}
+                />
+            </AprrovalCard>
+            <AprrovalCard>
+                <CommentDetail
+                author='Janes' 
+                timeAgo='Yesterday at 4:45PM' 
+                content='I like the writing'
+                avatar={faker.image.image()}
+                />
+            </AprrovalCard>
         </div>
     )
 }
